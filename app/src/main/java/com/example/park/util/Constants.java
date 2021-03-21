18946d8aec8fe0f3c544a7171079c86a0499f94a
dragos DD <dragos.sandru99@gmail.com>
2021-03-21 10:54:26 +0200

    Create Constants class in Util package

---
+++ app/src/main/java/com/example/park/util/Constants.java
@@ -0,0 +1,11 @@
+package com.example.park.util;
+
+public class Constants {
+
+    public static final int PERMISSIONS_REQUEST_ENABLE_GPS = 101;
+    public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 102;
+
+    public static final int ERROR_DIALOG_REQUEST = 201;
+
+
+}
