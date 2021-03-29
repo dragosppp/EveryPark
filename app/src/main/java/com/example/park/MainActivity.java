d0ca94ef1e0cbd901088f78bd79e0911c70a2233
dragos DD <dragos.sandru99@gmail.com>
2021-03-29 15:15:58 +0300

    getLastKnownLocation()

--- app/src/main/java/com/example/park/MainActivity.java
+++ app/src/main/java/com/example/park/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +20,11 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import static com.example.park.util.Constants.ERROR_DIALOG_REQUEST;
 import static com.example.park.util.Constants.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
@@ -26,24 +33,48 @@ import static com.example.park.util.Constants.PERMISSIONS_REQUEST_ENABLE_GPS;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private boolean fineLocationPermision = false;
+    private boolean fineLocationPermission = false;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    private void getLastKnownLocation() {
+        Log.d(TAG, "getLatsKnownLocation: OK");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if(task.isSuccessful()){
+                    Location location = task.getResult();
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    Log.d(TAG, "Geolocation: " + latLng.latitude + ", " + latLng.longitude);
+                }
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         if(checkMapServices()) {
-            if(fineLocationPermision) {
+            if(fineLocationPermission) {
                 getSupportFragmentManager().beginTransaction()
                         .setReorderingAllowed(true)
                         .add(R.id.map_container_view_fragment, MapsFragment.class, null)
                         .commit();
+                getLastKnownLocation();
+                //todo maybe move  getLastKnownLocation(); to the fragment
             }else{
                 getLocationPermission();
             }
@@ -92,7 +123,8 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-            fineLocationPermision = true;
+            fineLocationPermission = true;
+            getLastKnownLocation();
         } else {
             ActivityCompat.requestPermissions(this,
                     new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
@@ -125,13 +157,13 @@ public class MainActivity extends AppCompatActivity {
                                            @NonNull String permissions[],
                                            @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        fineLocationPermision = false;
+        fineLocationPermission = false;
         switch (requestCode) {
             case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    fineLocationPermision = true;
+                    fineLocationPermission = true;
                 }
             }
         }
@@ -143,11 +175,10 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onActivityResult: called.");
         switch (requestCode) {
             case PERMISSIONS_REQUEST_ENABLE_GPS: {
-                if (!fineLocationPermision) {
+                if (!fineLocationPermission) {
                     getLocationPermission();
                 }else{
-                    //ceva aici
-                    Toast.makeText(this, "Aplication will not work properly without Gps permission", Toast.LENGTH_SHORT).show();
+                    getLastKnownLocation();
                 }
             }
         }
