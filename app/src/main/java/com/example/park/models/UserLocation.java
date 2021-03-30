733de1a63330fb3414686ec1e1f1d66143ce39d1
dragos DD <dragos.sandru99@gmail.com>
2021-03-30 23:25:08 +0300

    UserLocation Class was created.

---
+++ app/src/main/java/com/example/park/models/UserLocation.java
@@ -0,0 +1,64 @@
+package com.example.park.models;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.ServerTimestamp;
+
+
+public class UserLocation {
+
+    private LatLng latLng;
+    private GeoPoint geoPoint;
+    private @ServerTimestamp String timestamp;
+    private User user;
+
+    public UserLocation(LatLng latLng, GeoPoint geoPoint, String timestamp, User user) {
+        this.latLng = latLng;
+        this.geoPoint = geoPoint;
+        this.timestamp = timestamp;
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "UserLocation{" +
+                "latLng=" + latLng +
+                ", geoPoint=" + geoPoint +
+                ", timestamp='" + timestamp + '\'' +
+                ", user=" + user +
+                '}';
+    }
+
+    public LatLng getLatLng() {
+        return latLng;
+    }
+
+    public void setLatLng(LatLng latLng) {
+        this.latLng = latLng;
+    }
+
+    public GeoPoint getGeoPoint() {
+        return geoPoint;
+    }
+
+    public void setGeoPoint(GeoPoint geoPoint) {
+        this.geoPoint = geoPoint;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
