25d4b4f9b3a1331e7ea88d0ed1999489d32f65d5
dragos DD <dragos.sandru99@gmail.com>
2021-04-01 20:38:54 +0300

    utils Check class created

---
+++ app/src/main/java/com/example/park/util/Check.java
@@ -0,0 +1,14 @@
+package com.example.park.util;
+
+public class Check {
+
+   public static boolean areStringsEqual(String s1, String s2){
+      return s1.equals(s2);
+   }
+
+   public static boolean isStringEmpty(String string) {
+      return string.equals("");
+
+   }
+
+}
