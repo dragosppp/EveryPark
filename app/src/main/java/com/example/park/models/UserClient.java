3a8a2bb189c459af3e639b1eb553caf4a3e53f42
dragos DD <dragos.sandru99@gmail.com>
2021-04-01 21:21:53 +0300

    UserClient class created.

---
+++ app/src/main/java/com/example/park/models/UserClient.java
@@ -0,0 +1,14 @@
+package com.example.park.models;
+
+public class UserClient {
+
+   private User user = null;
+
+   public User getUser() {
+      return user;
+   }
+
+   public void setUser(User user) {
+      this.user = user;
+   }
+}
