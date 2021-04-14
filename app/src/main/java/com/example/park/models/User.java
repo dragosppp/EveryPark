61ee3e88dd36dadb23a9a05ce81b00187185347e
dragos DD <dragos.sandru99@gmail.com>
2021-04-14 12:32:31 +0300

    Milestone: Geolocation to Firebase

--- app/src/main/java/com/example/park/models/User.java
+++ app/src/main/java/com/example/park/models/User.java
@@ -3,100 +3,100 @@ package com.example.park.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class User implements Parcelable{
+public class User implements Parcelable {
 
-    private String email;
-    private String userId;
-    private String username;
-    private String avatar;
+   private String email;
+   private String userId;
+   private String username;
+   private String avatar;
 
-    public User(String email, String userId, String username, String avatar) {
-        this.email = email;
-        this.userId = userId;
-        this.username = username;
-        this.avatar = avatar;
-    }
+   public User(String email, String userId, String username, String avatar) {
+      this.email = email;
+      this.userId = userId;
+      this.username = username;
+      this.avatar = avatar;
+   }
 
-    public User() {
+   public User() {
 
-    }
+   }
 
-    protected User(Parcel parcel) {
-        email = parcel.readString();
-        userId = parcel.readString();
-        username = parcel.readString();
-        avatar = parcel.readString();
-    }
+   protected User(Parcel parcel) {
+      email = parcel.readString();
+      userId = parcel.readString();
+      username = parcel.readString();
+      avatar = parcel.readString();
+   }
 
-    public static final Creator<User> CREATOR = new Creator<User>() {
-        @Override
-        public User createFromParcel(Parcel in) {
-            return new User(in);
-        }
+   public static final Creator<User> CREATOR = new Creator<User>() {
+      @Override
+      public User createFromParcel(Parcel in) {
+         return new User(in);
+      }
 
-        @Override
-        public User[] newArray(int size) {
-            return new User[size];
-        }
-    };
+      @Override
+      public User[] newArray(int size) {
+         return new User[size];
+      }
+   };
 
-    public String getAvatar() {
-        return avatar;
-    }
+   public String getAvatar() {
+      return avatar;
+   }
 
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
+   public void setAvatar(String avatar) {
+      this.avatar = avatar;
+   }
 
-    public static Creator<User> getCREATOR() {
-        return CREATOR;
-    }
+   public static Creator<User> getCREATOR() {
+      return CREATOR;
+   }
 
-    public String getEmail() {
-        return email;
-    }
+   public String getEmail() {
+      return email;
+   }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+   public void setEmail(String email) {
+      this.email = email;
+   }
 
-    public String getUserId() {
-        return userId;
-    }
+   public String getUserId() {
+      return userId;
+   }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
+   public void setUserId(String userId) {
+      this.userId = userId;
+   }
 
-    public String getUsername() {
-        return username;
-    }
+   public String getUsername() {
+      return username;
+   }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+   public void setUsername(String username) {
+      this.username = username;
+   }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "email='" + email + '\'' +
-                ", user_id='" + userId + '\'' +
-                ", username='" + username + '\'' +
-                ", avatar='" + avatar + '\'' +
-                '}';
-    }
+   @Override
+   public String toString() {
+      return "User{" +
+              "email='" + email + '\'' +
+              ", user_id='" + userId + '\'' +
+              ", username='" + username + '\'' +
+              ", avatar='" + avatar + '\'' +
+              '}';
+   }
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
+   @Override
+   public int describeContents() {
+      return 0;
+   }
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(email);
-        dest.writeString(userId);
-        dest.writeString(username);
-        dest.writeString(avatar);
-    }
+   @Override
+   public void writeToParcel(Parcel dest, int flags) {
+      dest.writeString(email);
+      dest.writeString(userId);
+      dest.writeString(username);
+      dest.writeString(avatar);
+   }
 }
 
--- app/src/main/java/com/example/park/util/Constants.java
+++ app/src/main/java/com/example/park/util/Constants.java
@@ -6,6 +6,10 @@ public class Constants {
     public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 102;
 
     public static final int ERROR_DIALOG_REQUEST = 201;
+    public static final String USER_LOCATION_KEY = "User Location";
 
+   public static final String MAIN_TAG = "User Location";
+   public static final String LOGIN_TAG = "User Location";
+   public static final String MAP_FRAGMENT_TAG = "User Location";
 
 }
